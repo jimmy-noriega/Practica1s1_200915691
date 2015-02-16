2b862265c455f9b500ff218baecd2527982292da
Jimmy Noriega <j.y.n14@hotmail.com>
2015-02-15 19:59:52 -0600

    Primeri commit para empaquetar todo lo que tngo que hacer para programar

---
+++ src/practica1_prueba/Nodo_REgitro_Estudiante.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_prueba;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo_REgitro_Estudiante {
+    String Nombre;
+    String Edad;
+    
+    String NodoCompleto = this.Nombre;
+
+
+    
+    
+    
+}
