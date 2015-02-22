eeebe78bcd1e0edfaeee0d04e19e83df1447aad5
Jimmy Noriega <j.y.n14@hotmail.com>
2015-02-21 18:03:36 -0600

    generacion de los primeros dos paneles de juego y estudio del lenguaje

---
+++ src/practica1_S1_2015/Implementacion_fondos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_S1_2015;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Implementacion_fondos extends javax.swing.JPanel {
+    
+    public Implementacion_fondos(){
+    initComponents();
+    }
+    
+    public void paintComponent(Graphics g){
+        Dimension Tamaño= getSize();
+        ImageIcon imagenfondo = new ImageIcon(getClass().getResource("imagenes/fondo de juego.png"));
+        g.drawImage(imagenfondo.getImage(),0,0,Tamaño.width,Tamaño.height,null);
+        setOpaque(false);
+        super.paintComponent(g);
+        
+    }
+    
+    
+
+    private void initComponents() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+    
+    
+    
+}
--- src/practica1_S1_2015/PRACTICA1_PRUEBA.java
+++ src/practica1_S1_2015/PRACTICA1_PRUEBA.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practica1_prueba;
+package practica1_S1_2015;
 
 /**
  *
